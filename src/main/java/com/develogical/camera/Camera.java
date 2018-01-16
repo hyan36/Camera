f801768870229b61f041714912acd2c82f4139de
Haihao Yan <hyan36@gamil.com>
2018-01-16 16:02:08 +0000

    Sensor powerup

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -2,12 +2,19 @@ package com.develogical.camera;
 
 public class Camera {
 
+    Sensor sensor;
+
+    public Camera(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
     public void pressShutter() {
         // not implemented
     }
 
     public void powerOn() {
         // not implemented
+        this.sensor.powerUp();
     }
 
     public void powerOff() {
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -6,8 +6,17 @@ import org.mockito.ArgumentCaptor;
 import static org.mockito.Mockito.*;
 
 public class CameraTest {
+
+
+    Sensor sensor = mock(Sensor.class);
+    Camera myCamera = new Camera(sensor);
+
+
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
         // write your test here
+        myCamera.powerOn();
+        verify(sensor).powerUp();
     }
+
 }
